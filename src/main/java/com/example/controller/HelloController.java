f8f8644d9ded7e6e94d8918ecd56d9233bac6a60
edensiboni <edensiboni5@gmail.com>
2025-05-13 12:18:43 +0300

    changed port to 8081
    {"status":"UP"}
    gets 404 in postman

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -10,17 +10,15 @@ public class HelloController {
 
     @PostMapping("/echo")
     public ResponseEntity<Message> echoMessage(@RequestBody Message input) {
-        // Create a new Message with the input content and "I did it" on a new line
-        String userInpuString = input.getContent();
+        String userInputString = input.getContent();
         String statusCode = "200";
 
-        if (userInpuString == null || userInpuString.trim().isEmpty()) {
-
+        if (userInputString == null || userInputString.trim().isEmpty()) {
             statusCode = "400";
             return ResponseEntity.badRequest().body(new Message("Please provide 'content' in the request.", statusCode));
-
         }
+
         String modifiedContent = input.getContent() + "\nI did it";
-        return ResponseEntity.ok(new Message(modifiedContent,statusCode));
+        return ResponseEntity.ok(new Message(modifiedContent, statusCode));
     }
 }
